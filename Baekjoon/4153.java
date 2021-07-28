cff8c697f18251e1e5f81f8a84f57371867e8551
Mint_Candy <76831873+Mint-Candy95@users.noreply.github.com>
2021-07-28 14:37:06 +0900

    Create 4153.java

---
+++ Baekjoon/4153.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        
+        while(true){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int temp;
+            if(a == 0 && b==0 && c==0) break;
+            
+            if(a>c){
+                temp = a;
+                a = c;
+                c = temp;
+            }
+            if(b>c){
+                temp = b;
+                b = c;
+                c = temp;
+            }
+        
+            if(a*a+b*b == c*c){
+                System.out.println("right");
+            }else{
+                System.out.println("wrong");
+            }
+        }
+    }
+}
