34cafd729936f60eca9d015b83d4302b14f28466
Mint_Candy <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-03 01:17:25 +0900

    Create 1110_더하기 사이클.java

---
+++ Baekjoon/1110_더하기 사이클.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int count = 0;
+        int temp = N;
+        int sum = 0;
+        do{
+            count ++;
+            sum = temp/10+temp%10;
+            temp = temp%10*10 + sum%10;
+        }while(N!=temp);
+        
+        bw.write(Integer.toString(count));
+        
+        bw.flush();
+        bw.close();
+        br.close();
+        
+    }
+}
