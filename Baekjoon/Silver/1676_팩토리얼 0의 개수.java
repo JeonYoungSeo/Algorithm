347067f8c4fd03c612af5f52e1d83b3cdfd9a979
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-15 09:14:13 +0900

    Create 1676_팩토리얼 0의 개수.java

---
+++ Baekjoon/Silver/1676_팩토리얼 0의 개수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine()); // N
+		
+		int result = fibo(N);
+			
+		bw.write(result+" ");
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static int fibo(int n) {
+		if(n==0) return 0; 
+				
+		int result = 0;
+		int temp = n;
+		while(temp%5==0) {
+			temp /= 5;
+			result ++;
+		}
+		
+		return result + fibo(n-1);
+	}
+}
