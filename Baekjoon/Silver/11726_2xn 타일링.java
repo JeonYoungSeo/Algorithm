5ad923c0e53cfb047a1190c924451be064d3fed1
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-15 15:44:38 +0900

    Create 11726_2xn 타일링.java

---
+++ Baekjoon/Silver/11726_2xn 타일링.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static int N;
+	static int memo[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine()); // N
+		memo = new int[1001];
+		memo[1] = 1;
+		memo[2] = 2;
+		int total_count = tiling(N);
+			
+		bw.write(total_count+"\n");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static int tiling(int n) {
+		//기저 조건
+		if(n==1) {
+			return 1;
+		}else if(n==2) {
+			return 2;
+		}
+		
+		if(memo[n]!=0) return memo[n];
+		
+		memo[n] = (tiling(n-2)+tiling(n-1))%10007;
+		
+		return memo[n];
+	}
+}
