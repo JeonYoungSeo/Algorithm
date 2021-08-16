40277d076e16b7d60ef694115fa27f68794f0b49
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-16 14:26:04 +0900

    Create 11047_동전 0.java

---
+++ Baekjoon/Silver/11047_동전 0.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] money = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			money[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int count = 0;
+		
+		for(int i=N-1; i>=0; i--) {
+			if(K>=money[i]) {
+				count += K/money[i];
+				K = K%money[i];
+			}
+		}
+		
+		bw.write(count+"\n");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
