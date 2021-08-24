3b7df923b12085be455f3683d560545677238973
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-24 13:55:34 +0900

    Update 3289_서로소 집합.java

--- SWEA/D4/3289_서로소 집합.java
+++ SWEA/D4/3289_서로소 집합.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -38,11 +35,9 @@ public class Main{
 		
 		parents[bRoot] = aRoot;
 		
-		
 		return true;
 		
 	}
-	
 	
 	public static void main(String[] args) throws IOException
 	{
