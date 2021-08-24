a5d309365665786d8ef4f873eca628b71ab1b1b6
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-24 13:53:25 +0900

    Create 1717_집합의 표현.java

---
+++ Baekjoon/Gold/1717_집합의 표현.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Author : YoungSeo Jeon
+ * Date : 2021-08-24
+ * Description : 백준 1717
+ */
+
+public class Main{
+	static int N; //원소 개수
+	static int[] parents; //부모원소 관리
+	
+	//모든원소를 자신을 대표자로 만듦
+	private static void make() {
+		for(int i=0; i<N+1; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	private static int find(int a) {
+		if(a==parents[a]) return a;//자신이 대표자
+		return parents[a] = find(parents[a]); //자신이 속한 집하의 대표자를 자신의 부모로 : path compression
+		
+	}
+	
+	//두 원소를 하나의 집합으로 합치기
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		
+		
+		return true;
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		parents = new int[N+1]; 
+		make();
+		int M = Integer.parseInt(st.nextToken());
+		
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			//합의 연산인지 출력인지 , 첫번째 원소, 두번째 원소
+			int c = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			//합하기
+			if(c==0) {
+				union(a,b);
+			}else { //출력부분
+				int aRoot = find(a);
+				int bRoot = find(b);
+				if(aRoot==bRoot)
+					System.out.println("YES");
+				else
+					System.out.println("NO");
+			}
+		}
+	}
+}
