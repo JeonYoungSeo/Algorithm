5d6d2b722617b656cd63fcaa281e873e33490488
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-08-29 19:08:43 +0900

    Create 15904_UCPC는 무엇의 약자일까?.java

---
+++ Baekjoon/Silver/15904_UCPC는 무엇의 약자일까?.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Author : YoungSeo Jeon
+ * Date : 2021-08-29
+ * Description : 백준 15904
+ */
+
+public class Main{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		
+		char[] result = {'U','C','P','C'};
+		int chk = 0;
+		
+		for(int i=0; i<str.length(); i++) {
+			
+			if(result[chk]==str.charAt(i)) {
+				chk++;
+			}
+			if(chk==4){
+				System.out.println("I love UCPC");
+				return;
+			}
+		}
+		System.out.println("I hate UCPC");
+		
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
