e31b0b7d6ca73bf398bbfece2cd6828c49b32ae2
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-09-13 21:39:19 +0900

    Create 15657_N과 M(8).java

---
+++ Baekjoon/Silver/15657_N과 M(8).java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * Author : YoungSeo Jeon
+ * Date : 2021-09-13
+ * Description : 백준 15657
+ */
+
+public class Main{
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int M, N ;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++) 
+        	arr[i] = Integer.parseInt(st.nextToken());
+        
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+		permutation(sb,0,0);
+        
+        bw.flush();
+        br.close();
+        bw.close();
+	}
+	
+	public static void permutation(StringBuilder sb,int n,int curr) throws IOException {
+		if(n == N) {
+			bw.write(sb.toString()+"\n");
+			return ;
+		}
+		
+		for(int i=curr; i<M; i++) {
+			sb.append(arr[i]+" ");
+			permutation(sb,n+1,curr);
+			sb.setLength(sb.length()-1);
+			if(sb.lastIndexOf(" ")!=-1) {
+				sb.setLength(sb.lastIndexOf(" "));
+				sb.append(" ");
+			}else {
+				sb.setLength(0);
+			}
+			
+			 curr++;
+		}
+	}
+}
