88da239b45acf5f9f23b25db8957a3809bbe4f3a
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-10-17 14:25:57 +0900

    Create 1976_여행 가자.java

---
+++ Baekjoon/Gold/1976_여행 가자.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+백준 1976 문제 
+
+플로이드-와샬을 적용하면 간단하게 풀립니다.
+*/
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[][] able = new int[N][N]; 
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				able[i][j] = Integer.parseInt(st.nextToken());
+				if(i==j) able[i][j] = 1;
+			}
+		}
+		
+		//플로이드 와샬 
+		for(int k=0; k<N; k++) {
+			for(int i=0; i<N; i++) {
+				if(k==i) continue;
+				for(int j=0; j<N; j++) {
+					if(j==k || j==i) continue;
+					
+					if(able[i][k]+able[k][j]==2) {
+						able[i][j] = 1;
+					}
+				}
+			}
+		}
+		
+		boolean result = true;
+		
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		
+		int start = Integer.parseInt(st.nextToken());
+		int end = 0;
+		for(int i=0; i<M-1; i++) {
+			end = Integer.parseInt(st.nextToken());
+			
+			if(able[start-1][end-1]==0) {
+				result = false;
+				break;
+			}
+			start = end;
+		}
+		
+		if(result) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}
+	}
+}
