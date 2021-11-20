f37d00a436aa533e69b8c54700259a906f25f212
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-11-21 04:12:34 +0900

    Create 2443_별 찍기 - 6.java

---
+++ Baekjoon/Bronze/2443_별 찍기 - 6.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<2*(N-1-i)+1; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+}
