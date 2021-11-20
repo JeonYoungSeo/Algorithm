88eed35d8cd2c3e51c894a1b185cd4dc50481bca
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-11-21 04:50:01 +0900

    Create 10990_별 찍기 - 15.java

---
+++ Baekjoon/Bronze/10990_별 찍기 - 15.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        int cnt = -1;
+        for(int i=1; i<=N; i++){
+
+            for(int j=1; j<=N-i; j++)
+                sb.append(' ');
+
+            sb.append('*');
+
+            if(i==1) {
+                sb.append('\n');
+                continue;
+            }
+            cnt += 2;
+            for(int j=1; j<=cnt; j++)
+                sb.append(' ');
+
+            sb.append('*');
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+}
