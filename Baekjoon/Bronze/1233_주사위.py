7a8b059f9cea1c5354e37507b173dad311f22822
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2021-11-21 18:13:39 +0900

    Create 1233_주사위.py

---
+++ Baekjoon/Bronze/1233_주사위.py
@@ -0,0 +1,10 @@
+s1, s2, s3 = map(int, input().split())
+
+lst = [0, ]*(s1+s2+s3+1)
+
+for i in range(1, s1+1) :
+    for j in range(1, s2+1) :
+        for k in range(1, s3+1) :
+            lst[i+j+k] += 1
+
+print(lst.index(max(lst)))
