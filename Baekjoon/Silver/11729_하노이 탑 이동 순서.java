c3682d232757bd0728bd44ce717e121cf8e5fbc2
Hakka_Ame <76831873+Mint-Candy95@users.noreply.github.com>
2022-01-14 21:09:44 +0900

    Update 11729_하노이 탑 이동 순서.java

--- Baekjoon/Silver/11729_하노이 탑 이동 순서.java
+++ Baekjoon/Silver/11729_하노이 탑 이동 순서.java
@@ -9,33 +9,31 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         subResult = new StringBuilder();
-        //
+        
         int result = hanoi(1,3,2,N);
-
-
+        
         System.out.println(result);
         System.out.println(subResult.toString());
     }
 
 
-
-    private static int hanoi(int from,int to, int mid,int N) {
+                              //어디에서~로     경유지   몇층짜리인지
+private static int hanoi(int from,int to, int mid,int N) {
 
         if(N==1) {
             subResult.append(from+" "+to+"\n");
 
             return 1;
         }
-
+         //옮긴 횟수
         int result = 1;
-
+        
         result += hanoi(from,mid,to,N-1);
 
         subResult.append(from+" "+to+"\n");
 
         result += hanoi(mid,to,from, N-1);
-
-        //
+  
         return result;
     }
 
