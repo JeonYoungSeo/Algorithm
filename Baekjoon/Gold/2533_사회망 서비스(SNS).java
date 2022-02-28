1764a6fe8ff00361fbfc53a22240e6f4da95ebb3
HakkaAme <76831873+JeonYoungSeo@users.noreply.github.com>
2022-02-28 22:13:17 +0900

    Create 2533_사회망 서비스(SNS).java

---
+++ Baekjoon/Gold/2533_사회망 서비스(SNS).java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static class Node{
+        int next;
+        Node Nnode;
+
+        Node(int next, Node Nnode){
+            this.next = next;
+            this.Nnode = Nnode;
+        }
+    }
+
+    static boolean[] visited;
+    static int result ;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Node[] nodes = new Node[N+1];
+        visited = new boolean[N+1];
+        while(N-->1){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            nodes[u] = new Node(v, nodes[u]);
+            nodes[v] = new Node(u, nodes[v]);
+        }
+        int start = 1;
+        result = 0;
+        dfs(start, nodes);
+        System.out.println(result);
+
+    }
+    private static boolean dfs(int curr, Node[] nodes) {
+
+        visited[curr]= true;
+        boolean check = false;
+        boolean leaf = true;
+        int all = 0;
+        int ear = 0;
+        for(Node itr=nodes[curr]; itr!=null; itr=itr.Nnode){
+            if(visited[itr.next]) continue;
+            visited[itr.next] = true;
+            leaf=false;
+            all++;
+            if(dfs(itr.next, nodes)){
+                check = true;
+                ear++;
+            }
+
+        }
+
+        if(all!=ear && !leaf){
+            result++;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
