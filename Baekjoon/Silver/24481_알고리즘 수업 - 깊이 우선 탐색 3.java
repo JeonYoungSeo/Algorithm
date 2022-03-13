84524688fa25d3235a2c2cc9648fa65d7e862fd5
HakkaAme <76831873+Mint-Candy95@users.noreply.github.com>
2022-03-13 22:20:13 +0900

    Create 24481_알고리즘 수업 - 깊이 우선 탐색 3.java

---
+++ Baekjoon/Silver/24481_알고리즘 수업 - 깊이 우선 탐색 3.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] result;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer>[] que = new PriorityQueue[N+1];
+        for(int i=1; i<=N; i++){
+            que[i] = new PriorityQueue<>();
+        }
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            que[u].add(v);
+            que[v].add(u);
+        }
+        result = new int[N+1];
+        Arrays.fill(result, -1);
+        visited = new boolean[N+1];
+        dfs(que, 0,R);
+        StringBuilder answer = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            answer.append(result[i]+"\n");
+        }
+        System.out.println(answer);
+
+    }
+
+    private static void dfs(PriorityQueue<Integer>[] que, int dep, int R) {
+        visited[R] = true;
+        result[R] = dep;
+        while(!que[R].isEmpty()){
+            int curr = que[R].poll();
+
+            if(visited[curr]) continue;
+
+            dfs(que, dep+1, curr);
+        }
+    }
+
+
+}
