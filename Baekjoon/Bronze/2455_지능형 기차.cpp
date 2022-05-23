20375bb0d05b973f9120dd4094da2f18e50c00fd
HakkaAme <76831873+Mint-Candy95@users.noreply.github.com>
2022-05-23 15:32:58 +0900

    Create 2455_지능형 기차.cpp

---
+++ Baekjoon/Bronze/2455_지능형 기차.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int in, out ;
+
+    int answer = 0;
+    int people = 0;
+
+    for(int i=0; i<4; i++){
+        cin >> out >> in;
+
+        people += in;
+        people -= out;
+
+        answer = (answer<people)? people: answer;
+
+    }
+
+    cout << answer << endl;
+}
